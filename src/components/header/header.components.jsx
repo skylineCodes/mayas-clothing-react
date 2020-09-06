2a1d635e59db291abf0b2ebebc11493bcb1e7c74
SkylineCodes <onakoyak@gmail.com>
2020-09-06 20:13:49 +0100

    Added cart-icon and cart-dropdown

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.components';
+import CartDropdown from '../cart-dropdown/cart-dropdown.components';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -21,18 +23,23 @@ const Header = ({ currentUser }) => (
             </Link>
             {
                 currentUser ? 
-                <div className="option" onClick={ () => auth.signOut() }>SIGN OUT</div>
+                (<div className="option" onClick={ () => auth.signOut() }>SIGN OUT</div>)
                 :
-                <Link className="option" to='/signin'>
+                (<Link className="option" to='/signin'>
                     SIGN IN
-                </Link>
+                </Link>)
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : < CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
