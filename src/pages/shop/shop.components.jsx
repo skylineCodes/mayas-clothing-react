a048fddf0ece6436afd2de143e7a255234f3d386
SkylineCodes <onakoyak@gmail.com>
2020-09-10 19:08:40 +0100

    Updated directory and shop data to its own reducer, Added Collection/Category feature, Added an advanced routing to the collection route

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
+import CollectionOverview from '../../components/collections-overview/collections-overview.components';
+import CollectionPage from '../collection/collection.components';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.components'
-
-class Shop extends React.Component {
-    constructor(props) {
-        super();
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherSectionProps }) => (
-                        <CollectionPreview key={id} {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const Shop = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
 export default Shop;
